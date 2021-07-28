f5531e56d3ea6ec592bdc1f4b3ca1bb4c43ad2a9
janetto-cornetto <80689902+janetto-cornetto@users.noreply.github.com>
2021-07-28 13:26:21 +1000

    Update bowling_submission.rb

--- 04.bowling/bowling_submission.rb
+++ 04.bowling/bowling_submission.rb
@@ -3,15 +3,13 @@ score = ARGV[0]
 
 #1投毎に分割する
 scores = score.split(',')
-shots = [] # shots - Array.new
+shots = [] 
 
 #数字に変換
 scores.each do |s|
   if s == 'X'
     shots << 10
     shots << nil
-  elsif s == 'S' #spare
-    shots << 10
   else
     shots << s.to_i
   end
@@ -32,22 +30,22 @@ front_frame.push(last_frame) #()使ってもスペースでもおｋ
 #p front_frame
 
 point = 0
-p front_frame #あるべき形のフレーム
+front_frame #あるべき形のフレーム
 front_frame.each.with_index do |frame, i|
-    frame_point = 0
-    if i == 9
+frame_point = 0
+if i == 9
+    frame_point = frame.sum
+else 
+    if frame[0] == 10 #strike
+        frame_point = (10 + frames[i+1][0])
+        frame_point += frames[i+1][1] || frames[i+2][0] #
+    elsif frame.sum == 10 #spare
+        frame_point = frames[i+1][0] + 10
+    else  #normal
         frame_point = frame.sum
-    else 
-        if frame[0] == 10 #strike
-            frame_point = (10 + frames[i+1][0])
-            frame_point += frames[i+1][1] || frames[i+2][0] #
-        elsif frame.sum == 10 #spare
-            frame_point = frames[i+1][0] + 10
-        else  #normal
-            frame_point = frame.sum
-        end
-    end 
-    point += frame_point 
+    end
+end 
+point += frame_point 
 end 
 
 p point
